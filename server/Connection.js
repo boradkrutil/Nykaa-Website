9802046f40bcf49e2152731e04b362033d50cb96
Borad Krutil Vipulbhai <124248935+boradkrutil@users.noreply.github.com>
2024-09-30 17:11:02 +0530

    Update Connection.js

--- server/Connection.js
+++ server/Connection.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose")
 
 const ConnectDb = async() =>{
    try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/Nykaa")
+    await mongoose.connect("mongodb+srv://krutilborad2020:Krutil1230@cluster0.zqtlcs5.mongodb.net/")
     console.log("ConnectDb");
    } catch (error) {
     console.log(error);
